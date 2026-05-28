7129de9d8aca93c8892fa8ac81861be7fc697d25
Gella sri vignesh <155979128+vigneshgella@users.noreply.github.com>
2026-05-28 16:13:56 +0530

    Add files via upload

---
+++ ContextExample/src/main/java/com/context/FirstServlet.java
@@ -0,0 +1,23 @@
+package com.context;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+public class FirstServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		ServletContext context = getServletContext();
+		String d = context.getInitParameter("driver");
+		String u = context.getInitParameter("url");
+		out.print("<h1>This is First Servlet Team</h1>");
+		out.print("<h1>Driver is "+d+"</h1>");
+		out.print("<h1>Url is "+u+"</h1>");
+	}
+
+}
